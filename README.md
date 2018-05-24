a78b676e2a202c2c19aaf2ba84273f479ab3bf78
Cameron Kingsbury <camsbury7@gmail.com>
2018-05-23 21:32:21 -0400

    feat: parse basic text
    Parse HTML into a basic response. Handle unicode with specific font?

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # etymology-of-word
 Display the etymology of a word in Emacs.
+
+Heavily influenced by [define-word](https://github.com/abo-abo/define-word), this package pulls in the etymology of the word from the wonderful [Online Etymology Dictionary](https://www.etymonline.com).
---
+++ etymology-of-word.el
@@ -0,0 +1,24 @@
+;;; etymology-of-word.el --- Display the etymology of a word in Emacs. -*- lexical-binding: t -*-
+
+(require 'url-parse)
+(require 'url-http)
+(require 'dash)
+
+;;;###autoload
+(defun etymology-of-word (word)
+  "Provide etymology of WORD from the Online Etymology Dictionary"
+  (interactive (list (read-string "Word: ")))
+  (let ((link (concat "https://etymonline.com/word/" (downcase word))))
+    (url-retrieve
+     link
+     (lambda (status)
+       (re-search-forward "<object>")
+       (setq beg (point))
+       (re-search-forward "</object>")
+       (setq end (point))
+       (->> (buffer-substring beg end)
+           (replace-regexp-in-string "<.*?>" "")
+           (replace-regexp-in-string "&quot;" "\"")
+           (message))))))
+
+(provide 'etymology-of-word)
